28ad49ae59d86fe8b291b76d3b0fb9ecbbeed621
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-10-20 23:41:38 -0300

    feat: Added Map component props and test

--- src/components/Map/test.tsx
+++ src/components/Map/test.tsx
@@ -12,4 +12,30 @@ describe('<Map />', () => {
       })
     ).toBeInTheDocument()
   })
+
+  it('Should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Petrópolis',
+      slug: 'petropolis',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+
+    const placeTwo = {
+      id: '2',
+      name: 'Murici',
+      slug: 'murici',
+      location: {
+        latitude: 129,
+        longitude: -50
+      }
+    }
+
+    render(<Map places={[place, placeTwo]} />)
+    expect(screen.getByTitle(/Petrópolis/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/Murici/i)).toBeInTheDocument()
+  })
 })
