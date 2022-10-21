501e4b4b21d0b09eaffacb70878fd1abe4c2b426
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-10-20 23:54:34 -0300

    refactor: Change link to document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,13 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
-          integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
-          crossOrigin=""
-        />
-
         <meta
           name="description"
           content="A simple project to show my favorites spots in the World"
