88755750855cfb7a1a3a19a81b124d49eb9c907c
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-10-22 17:16:16 -0300

    feat: Added get page by slug query

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,9 +2,10 @@ import { GraphQLClient } from 'graphql-request'
 
 const endpoint = process.env.GRAPHQL_HOST || ''
 
+console.log(process.env.GRAPHQL_TOKEN)
 const client = new GraphQLClient(endpoint, {
   headers: {
-    authorization: `Beared ${process.env.GRAPHQL_TOKEN}`
+    authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`
   }
 })
 
