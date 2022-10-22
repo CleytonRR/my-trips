a78bbb84adf658d0bca00e80396c65cb8a7662d4
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-10-21 22:32:51 -0300

    refactor: Fix right position

--- src/components/LinkWrapper/styles.ts
+++ src/components/LinkWrapper/styles.ts
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
   position: fixed;
   z-index: 1100; // bigger than leaflet
   top: var(--medium);
+  right: var(--medium);
   color: var(--white);
   cursor: pointer;
 
