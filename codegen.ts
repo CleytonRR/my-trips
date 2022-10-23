15d7b0f1cc8cec03fcb52bd96c33cd43b6a00184
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-10-22 22:10:10 -0300

    feat: Install and config codegen

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema:
+    'https://api-sa-east-1.hygraph.com/v2/cl9k6o0av2xxt01uhh3802yav/master',
+  documents: 'src/graphql/queries.ts',
+  generates: {
+    'src/graphql/generated/': {
+      preset: 'client',
+      plugins: []
+    }
+  }
+}
+
+export default config
