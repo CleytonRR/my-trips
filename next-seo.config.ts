3eaff8a344b122d43f907eab53ef93310ed34817
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-11-04 22:18:27 -0300

    feat: Config next-seo

---
+++ next-seo.config.ts
@@ -0,0 +1,15 @@
+const openGraphConfig = {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://my-trips.willianjusten.com.br',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@Willian_justen',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
+
+export default openGraphConfig
